3866a93eae80f6cdf3596c7d5569c459727237f2
skasero <spencerkkr@gmail.com>
2019-11-03 18:18:05 -0800

    Fixed Calculate loss

--- driver.py
+++ driver.py
@@ -2,5 +2,9 @@ import numpy as np
 import sklearn as sk
 import matplotlib.pyplot as plt
 import perceptron as pt
+import neural_network as nn
 
-print("hello world")
+
+X = np.array([[-2, 1], [1, 1], [1.5, -0.5], [-2, -1], [-1, -1.5], [2, -2]])
+Y = np.array([[1], [1], [1], [-1], [-1], [-1]])
+b = nn.build_model(X, Y, 4)
